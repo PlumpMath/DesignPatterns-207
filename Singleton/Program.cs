0e9c6e1645dafa16c3ea2300fe6eea43fa0687ce
Ankit Goyal <ankitgoyal.bpl@hotmail.com>
2016-11-14 21:05:23 -0800

    Singleton Class Description

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -10,7 +10,19 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
-            SingletonClassA.Instance
+            //  Difference between Singleton and Static classes.
+            //  1. Singleton object stores in Heap but, static object stores in stack
+            //  2. We can clone the object of Singleton but, we can not clone the static class object
+            //  3. Singleton class follow the OOP(object oriented principles) but not static class
+            //  4. We can implement interface with Singleton class but not with Static class.
+            
+            //  Both Singleton and Static classes can be thread-safe (or not) if they are properly implemented.
+
+            //  Links:
+            //  https://en.wikibooks.org/wiki/Computer_Science_Design_Patterns/Singleton
+            //  https://sourcemaking.com/design_patterns/singleton
+            //  http://stackoverflow.com/questions/519520/difference-between-static-class-and-singleton-pattern
+            //  http://csharpindepth.com/Articles/General/Singleton.aspx
         }
     }
 }
