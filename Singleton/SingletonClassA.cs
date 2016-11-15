6a1dd7057b84fe94201ef47a3485ffc68952ad71
Ankit Goyal <ankitgoyal.bpl@hotmail.com>
2016-11-14 19:55:34 -0800

    Singleton Class

---
+++ Singleton/SingletonClassA.cs
@@ -0,0 +1,24 @@
+﻿// Singleton Class
+// Three main points:
+//      1. Global Access Point to the Resource
+//      2. Single instance of the class 
+
+namespace Singleton
+{
+    // This class instance is not lazy initialized 
+    // and is highly useful in creating Logging libraries.  
+    public sealed class SingletonClassA
+    {
+        // Static Readonly Instance of the class provides 
+        // thread-safe creation of the instance
+        // Public modifier creates a Global access point 
+        // for the class instance
+        public static readonly SingletonClassA Instance = new SingletonClassA();
+
+        // Private (or Protected) Constructor ensures the 
+        // ownership of the instance of the Class. 
+        private SingletonClassA()
+        {
+        }
+    }
+}
---
+++ Singleton/SingletonClassB.cs
@@ -0,0 +1,51 @@
+﻿// Singleton Class
+// Three main points:
+//      1. Global Access Point to the Resource
+//      2. Single instance of the class 
+
+using System;
+
+namespace Singleton
+{
+    // This class is lazy initialized and thread safe during
+    // the creation of an instance
+    public sealed class SingletonClassB
+    {
+        // Static Object ensures the global life-time of the 
+        // instance of the class.
+        private static SingletonClassB classInstance = null;
+
+        // For Synchronization
+        private readonly static Object lockObject = new Object();
+
+        // Private (or Protected) Constructor ensures the 
+        // ownership of the instance of the Class. 
+        private SingletonClassB()
+        {
+        }
+
+        // Public Static function to provide Global Access to
+        // the instance of the class. 
+        // This function also ensures the Lazy Initialization 
+        // of the class Instance
+        public static SingletonClassB Instance()
+        {
+            if (classInstance != null)
+            {
+                return classInstance;
+            }
+            else
+            {
+                lock (lockObject)
+                {
+                    if (classInstance == null)
+                    {
+                        classInstance = new SingletonClassB();
+                    }
+                }
+
+                return classInstance;
+            }
+        }
+    }
+}
