d2a39466cfb356408fe6ce104730349c56fe84f2
Ankit Goyal <ankitgoyal.bpl@hotmail.com>
2016-11-21 12:17:14 -0800

    Factory Method Pattern

---
+++ FactoryMethod/FacultyFactory.cs
@@ -0,0 +1,86 @@
+﻿// Factory Method to provide a way of 
+// creating an instance of Faculty as 
+// per requirements. 
+// We need to define only the classes and their
+// functionality and we can create instances of
+// the required class during run-time.
+
+namespace FactoryMethod
+{
+    // Abstract Class for the Faculty
+    abstract class Faculty
+    {
+        public abstract string Title
+        {
+            get;
+        }
+    }
+
+    class Professor : Faculty
+    {
+        public override string Title
+        {
+            get
+            {
+                return "Professor";
+            }
+        }
+    }
+
+    class Lecturer : Faculty
+    {
+        public override string Title
+        {
+            get
+            {
+                return "Lecturer";
+            }
+        }
+    }
+
+    class Scientist : Faculty
+    {
+        public override string Title
+        {
+            get
+            {
+                return "Scientist";
+            }
+        }
+    }
+
+    enum FacultyType
+    {
+        Professor,
+        Lecturer,
+        Scientist
+    }
+
+    class FacultyFactory
+    {
+        private FacultyFactory()
+        {
+        }
+
+        public static Faculty GetFacultyInstance(FacultyType facultyType)
+        {
+            switch (facultyType)
+            {
+                case FacultyType.Professor:
+                    {
+                        return new Professor();
+                    }
+                case FacultyType.Lecturer:
+                    {
+                        return new Lecturer();
+                    }
+                case FacultyType.Scientist:
+                    {
+                        return new Scientist();
+                    }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        //  When to use Factory Method Pattern:
+        //  1. When a class does not know which class of objects it must create.
+        //  2. A class specifies its sub-classes to specify which objects to create.
+        //  3. You have to create an object of any one of sub-classes depending on the data provided.
+
+        //  The base type of the instance is known at the compile time but,
+        //  the actual type of instance is known only at the run-time
+
+        static void Main(string[] args)
+        {
+            Faculty facultyInstance = FacultyFactory.GetFacultyInstance(FacultyType.Professor);
+            Console.WriteLine(facultyInstance.Title);
+        }
+    }
+}
